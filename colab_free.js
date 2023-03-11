20045714835114711562c11b0e769ddd26bf7a32
mr-aydemir <aydemir_emre65@hotmail.com>
2023-03-12 00:40:50 +0300

    fix

--- colab_free.js
+++ colab_free.js
@@ -1,7 +1,9 @@
 offline_count=0
+is_busy=false
 function clickVariables(){
     query_string="div.left-pane-top > div:nth-child(3) > paper-icon-button"
     document.querySelector(query_string).click()
+    is_busy=false
 }
 
 function clickRamMessageOK() {
@@ -14,18 +16,27 @@ function clickFiles(){
 }
 
 function sendMessage(message) {
-    data={type: "FROM_PAGE", text: message, offline_count:offline_count}
+    data={type: "FROM_PAGE",  text: message, offline_count:offline_count}
     chrome.runtime.sendMessage(data);
 }
-
-
-function check_offline() {
-    if(!document.querySelector("body > div.notebook-vertical.colab-left-pane-open > div.notebook-horizontal > colab-left-pane")){
-        clickFiles()
+chrome.extension.onRequest.addListener(
+    function(request, sender, sendResponse) {
+        if(request.method == "getState"){
+            data={type: "FROM_PAGE",  text: message, offline_count:offline_count, method: "getState"}
+            sendResponse(data); //same as innerText
+            console.log(data);
+        }
     }
-    element=document.querySelector("colab-file-view > div.child-files > colab-file-view:nth-child(1) > div.file-title-row > span")
-    sendMessage("Kontrol ediliyor")
-    if (!element){
+);
+function is_offline() {
+    return !document.querySelector("colab-file-view > div.child-files > colab-file-view:nth-child(1) > div.file-title-row > span")
+}
+function is_left_pane_closed(params) {
+    return !document.querySelector("body > div.notebook-vertical.colab-left-pane-open > div.notebook-horizontal > colab-left-pane")
+}
+function doOnline() {
+    if (is_offline()){
+        is_busy=true
         offline_count++
         sendMessage("Çevrimdışı düzeltililiyor")
         clickVariables()
@@ -35,6 +46,26 @@ function check_offline() {
         setTimeout(clickFiles, 1000)
     }
     else sendMessage("Çevrimdışı Değil")
+}
+is_loading=false
+function check_offline() {
+    if (is_busy) return
+    if (is_loading){
+        sendMessage("Yükleniyor...")
+        if (document.querySelector("mwc-button")){
+            clickRamMessageOK()
+        }
+        if (is_offline()) return
+        is_loading=false
+    }
+    if(is_left_pane_closed()){
+        clickFiles()
+        if (is_offline()) {
+            is_loading=true
+        }
+        return
+    }
+    doOnline()
 }
 
 setInterval(check_offline, 1000)
--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 function injectTheScript() {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        // query the active tab, which will be only one tab
-        //and inject the script in it
         chrome.tabs.executeScript(tabs[0].id, { file: "colab_free.js" });
         document.getElementById('state').innerHTML = "Activated"
-        document.getElementById("clickactivity").disabled = true; 
+        document.getElementById("clickactivity").hidden = false
     });
 }
 document.getElementById('clickactivity').addEventListener('click', injectTheScript);
-
+function setText(request) {
+    if (request.type ||
+        request.type === "FROM_PAGE") {
+        document.getElementById('state').innerHTML = request.text
+        document.getElementById('offline_count').innerHTML = request.offline_count > 0 ? request.offline_count : ""
+    }
+}
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        if (request.type && request.type === "FROM_PAGE") {
-            document.getElementById('state').innerHTML = request.text
-            document.getElementById('offline_count').innerHTML = request.offline_count
-            sendResponse("ok");
-        }
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            if (sender.tab.id !== tabs[0].id)
+                return
+            setText(request)
+        });
     }
 );
+
+window.onload = function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        console.log(tabs[0])
+
+        if (!tabs[0].url.includes("colab")) {
+            document.getElementById('state').innerHTML = "Colab sitesinde değilsiniz"
+            document.getElementById("clickactivity").hidden = true
+        }
+        else {
+            document.getElementById('state').innerHTML = "Aktif Değil"
+            document.getElementById("clickactivity").hidden = false
+            chrome.tabs.sendRequest(tabs[0].id, { method: "getState" }, function (response) {
+                if (response.method !== "getState") return
+                setText(request)
+            })
+        }
+    });
+}
