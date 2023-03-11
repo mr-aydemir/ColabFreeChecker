e7eb2651da1c352088830cf2611e0ecb82ff3afa
mr-aydemir <aydemir_emre65@hotmail.com>
2023-03-11 20:58:19 +0300

    update style and messages

--- colab_free.js
+++ colab_free.js
@@ -1,6 +1,11 @@
+offline_count=0
 function clickVariables(){
     query_string="div.left-pane-top > div:nth-child(3) > paper-icon-button"
     document.querySelector(query_string).click()
+}
+
+function clickRamMessageOK() {
+    document.querySelector("mwc-button").click()
 }
 
 function clickFiles(){
@@ -8,16 +13,25 @@ function clickFiles(){
     document.querySelector(query_string).click()
 }
 
+function sendMessage(message) {
+    data={type: "FROM_PAGE", text: message, offline_count:offline_count}
+    chrome.runtime.sendMessage(data);
+}
+
+
 function check_offline() {
     element=document.querySelector("colab-file-view > div.child-files > colab-file-view:nth-child(1) > div.file-title-row > span")
-    console.log("Kontrol ediliyor")
+    sendMessage("Kontrol ediliyor")
     if (!element){
-        
-    console.log("offline düzeltililiyor")
+        offline_count++
+        sendMessage("Çevrimdışı düzeltililiyor")
         clickVariables()
-        setTimeout(clickFiles, 500)
+        if (document.querySelector("mwc-button")){
+            clickRamMessageOK()
+        }
+        setTimeout(clickFiles, 1000)
     }
-    else console.log("offline değil")
+    else sendMessage("Çevrimdışı Değil")
 }
 
 setInterval(check_offline, 1000)
--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-activated=false
 function injectTheScript() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         // query the active tab, which will be only one tab
         //and inject the script in it
-        chrome.tabs.executeScript(tabs[0].id, {file: "colab_free.js"});  
-        activated=true
-        stateElement.innerHTML="Activated"
+        chrome.tabs.executeScript(tabs[0].id, { file: "colab_free.js" });
+        document.getElementById('state').innerHTML = "Activated"
+        document.getElementById("clickactivity").disabled = true; 
     });
 }
 document.getElementById('clickactivity').addEventListener('click', injectTheScript);
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.type && request.type === "FROM_PAGE") {
+            document.getElementById('state').innerHTML = request.text
+            document.getElementById('offline_count').innerHTML = request.offline_count
+            sendResponse("ok");
+        }
+    }
+);
