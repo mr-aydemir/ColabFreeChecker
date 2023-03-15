82ae41c24143fd74851d6b971c38dbf3eed32050
mr-aydemir <aydemir_emre65@hotmail.com>
2023-03-15 18:25:10 +0300

    otomation system upgrade

--- js/colab_free.js
+++ js/colab_free.js
@@ -7,16 +7,16 @@ const file_browser_message_query = "body > div.notebook-vertical.large-notebook.
 const reload_files_query = "body > div.notebook-vertical.large-notebook.colab-left-pane-open > div.notebook-horizontal > colab-left-pane > colab-resizer > div.resizer-contents > div.left-pane-container > colab-file-browser > colab-file-tree > div.file-tree-buttons > paper-icon-button:nth-child(2)"
 const hidden_files_toggle = "#hidden-files-toggle"
 const search_query = "body > div.notebook-vertical.colab-left-pane-open > div.notebook-horizontal > colab-left-pane > div > div.left-pane-top > div:nth-child(2) > paper-icon-button"
-const connect_button_query="#top-toolbar > colab-connect-button"
+const connect_button_query = "#top-toolbar > colab-connect-button"
 
-state = "OFFLINE" // ["OFFLINE", "ONLINE", "LOADING", "DISABLED"]
+state = "OFFLINE" // ["OFFLINE", "ONLINE", "LOADING", "DISABLED", "NEXT_TAB"]
 activated = false
 offline_count = 0
 is_loading = false
 is_on_interaction = false
 
 function getState() {
-    if (!activated){
+    if (!activated) {
         return "DISABLED"
     }
     if (is_loading) {
@@ -51,6 +51,46 @@ function clickSearch() {
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+
+function sendMessage(nextstate, same_send = false) {
+    if (state == nextstate && !same_send) return
+    console.log(state)
+    state = nextstate
+    data = { type: "FROM_PAGE", state: state, offline_count: offline_count, activated: activated }
+    chrome.runtime.sendMessage(data);
+}
+
+function is_offline() {
+    return !document.querySelector(offline_query)
+}
+function is_left_pane_closed() {
+    return !document.querySelector(left_pane_query)
+}
+function shadowRoot() {
+    return document.querySelector(connect_button_query).shadowRoot
+}
+function connect_button() {
+    return shadowRoot().querySelector("#connect")
+}
+function statistics_is_valid() {
+    return connect_button.querySelector("#connect-button-resource-display")
+}
+function clickConnect() {
+    connect_button().click()
+}
+function is_there_gpu_allert_message() {
+    element = document.querySelector("body > mwc-dialog");
+    return element && element.shadowRoot.textContent.includes("GPU");
+}
+function recaptcha() {
+    return document.querySelector("#recaptcha-anchor > div.recaptcha-checkbox-border")
+}
+function drive_folder() {
+    return document.querySelector(".child-files>colab-file-view>.file-title-row>.file-tree-name[title='drive']")
+}
+
 is_on_interaction2 = false
 function doInteraction() {
     if (is_on_interaction2 || is_loading) return
@@ -66,44 +106,20 @@ function doInteraction() {
         }, 200)
     }, randomTime)
 }
-
-
-function sendMessage(nextstate, same_send=false) {
-    if (state==nextstate && !same_send) return
-    console.log(state)
-    state = nextstate
-    data = { type: "FROM_PAGE", state: state, offline_count: offline_count, activated: activated }
-    chrome.runtime.sendMessage(data);
-}
-
-function is_offline() {
-    return !document.querySelector(offline_query)
-}
-function is_left_pane_closed() {
-    return !document.querySelector(left_pane_query)
-}
-function shadowRoot() {
-   return document.querySelector(connect_button_query).shadowRoot
-}
-function connect_button() {
-    return shadowRoot().querySelector("#connect")
-}
-function statistics_is_valid() {
-        return connect_button.querySelector("#connect-button-resource-display")
-}
-function clickConnect() {
-    connect_button().click()
-}
-
+is_doing_online=false
 function doOnline() {
     /* if (!statistics_is_valid()){
         clickConnect()
     } */
-    if (is_offline()) {
+    if (is_offline() && !is_doing_online) {
         offline_count++
         sendMessage("OFFLINE")
         clickVariables()
-        clickFiles()
+        is_doing_online=true
+        setTimeout(function () {
+            clickFiles()
+            is_doing_online=false
+        }, 200)
         if (is_offline()) {
             is_loading = true
         }
@@ -111,10 +127,19 @@ function doOnline() {
     else sendMessage("ONLINE")
 }
 function check_offline() {
+    if (recaptcha()) {
+        recaptcha().click()
+    }
+    if (is_there_gpu_allert_message()) {
+        sendMessage("NEXT_TAB")
+        return
+    }
     if (is_on_interaction) return
     if (is_loading) {
         sendMessage("LOADING")
-        clickRamMessageOK()
+        if (!is_there_gpu_allert_message()) {
+            clickRamMessageOK()
+        }
         if (is_offline()) return
         is_loading = false
     }
@@ -131,6 +156,34 @@ function reset() {
     is_loading = false
     is_on_interaction = false
 }
+function set_enable(value) {
+    if (value) {
+        activeInterval = setInterval(function () {
+            check_offline()
+            doInteraction()
+        }, 1000)
+        activated = true
+    }
+    else {
+        clearInterval(activeInterval)
+        activeInterval = null
+        activated = false
+        reset()
+    }
+}
+function otomation() {
+    set_enable(true)
+    drive_folder_interval = setInterval(() => {
+        if (!drive_folder()) {
+            return
+        }
+        document.querySelector("colab-run-button").click()
+        clearInterval(drive_folder_interval)
+    }, 1000);
+}
+
+
+
 activeInterval = null
 chrome.runtime.onConnect.addListener(function (port) {
     if (port.name == "getState") {
@@ -140,22 +193,16 @@ chrome.runtime.onConnect.addListener(function (port) {
         });
     }
     else if (port.name == "toogleActivity") {
-        
         port.onMessage.addListener(function (response) {
             console.log("toogle");
-            if (response.enable) {
-                activeInterval = setInterval(check_offline, 1000)
-                activated = true
-            }
-            else {
-                clearInterval(activeInterval)
-                activeInterval = null
-                activated = false
-                reset()
-            }
-            
+            if (response.otomation) {
+                otomation()
+            } else
+                set_enable(response.enable)
             sendMessage(getState(), true)
         });
 
     }
 });
+//!document.querySelector("#message-area-secondary").ariaHidden && document.querySelector("#message-area-secondary").shadowRoot.textContent.includes("Drive")
+//document.querySelector("#recaptcha-anchor > div.recaptcha-checkbox-border").click()
--- pages/popup.js
+++ pages/popup.js
@@ -3,14 +3,15 @@ state_map = {
     "ONLINE": "Çevrimiçi",
     "LOADING": "Yükleniyor...",
     "WRONG_WEBSITE": "Colab sitesinde değilsiniz",
-    "DISABLED":"Aktif Değil"
+    "DISABLED": "Aktif Değil",
+    "NEXT_TAB": "Bir sonraki Tab'a geçildi"
 }
 
 function setText(request) {
     if (request.type ||
         request.type === "FROM_PAGE") {
         document.getElementById('state').innerHTML = state_map[request.state]
-        document.getElementById('offline_count').innerHTML = "Düzeltme: "+ request.offline_count
+        document.getElementById('offline_count').innerHTML = "Düzeltme: " + request.offline_count
         document.getElementById('toggle-input').checked = request.activated
     }
 }
@@ -20,12 +21,35 @@ function sendGetStateRequest(tabs) {
     port.postMessage({ url: tabs[0].url });
 }
 function on_toogle_click(cb) {
-    enable=this.checked
+    enable = this.checked
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         port = chrome.tabs.connect(tabs[0].id, { name: "toogleActivity" });
         port.postMessage({ enable: enable });
     });
 }
+function get_next_tab(tabsArray, current_tab) {
+    filtered = tabsArray.filter((x) => x.url.includes("colab"));
+    t_index = filtered.indexOf(filtered.filter((x) => x.id == current_tab.id)[0]);
+    last_index = filtered.length - 1;
+    t_next_index = t_index + 1
+    if(t_next_index > last_index) t_next_index=0
+    next_tab = filtered[t_next_index]
+    return next_tab
+}
+
+function go_nextTab(tab) {
+    chrome.tabs.query({ currentWindow: true }, function (tabsArray) {
+        if (tabsArray.length < 2) return;
+        next_tab=get_next_tab(tabsArray, tab)
+        console.log(tabsArray);
+        console.log(next_tab);
+        chrome.tabs.reload(tab.id);
+        chrome.tabs.update(next_tab.id, { active: true })
+        port = chrome.tabs.connect(next_tab.id, { name: "toogleActivity" });
+        port.postMessage({ enable: enable, otomation: true });
+    });
+}
+
 
 document.getElementById('toggle-input').addEventListener('change', on_toogle_click);
 chrome.runtime.onMessage.addListener(
@@ -35,6 +59,9 @@ chrome.runtime.onMessage.addListener(
                 return
             state = request
             setText(state)
+            if (request.state == "NEXT_TAB") {
+                go_nextTab(tabs[0])
+            }
         });
     }
 );
