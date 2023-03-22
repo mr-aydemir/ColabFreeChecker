a6ef6da3f63a0bc30cf30835a73baa0054a0b17e
mr-aydemir <aydemir_emre65@hotmail.com>
2023-03-22 19:35:35 +0300

    Update are_you_there_click.js

--- js/are_you_there_click.js
+++ js/are_you_there_click.js
@@ -1,7 +1,9 @@
 setInterval(function () {
+    if (document.querySelector("#rc-anchor-container")) {
+        console.log("are_you_there_click running...")
+    }
     document.querySelector("#rc-anchor-container")?.click()
-    console.log("are_you_there_click running...")
-}, 1000)
+}, 5000)
 
 window.addEventListener('beforeunload', function (event) {
     event.stopImmediatePropagation();
