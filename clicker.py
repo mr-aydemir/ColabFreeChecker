6399ef15e6f1099ba1b0ca9ab0a2ca8d61742cf9
mr-aydemir <aydemir_emre65@hotmail.com>
2023-03-25 20:01:11 +0300

    update

---
+++ clicker.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+import pyautogui as pg
+
+screenshot=pg.screenshot()
+screenshot=cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+
+robo=pg.locateOnScreen('light_robo.png')
+cv2.rectangle(screenshot, (robo.left, robo.top), (robo.left+robo.width, robo.top+robo.height), (0,255,255),2)
+
+cv2.imshow("robo", screenshot)
+
+cv2.waitKey(0)
--- js/are_you_there_click.js
+++ js/are_you_there_click.js
@@ -6,13 +6,26 @@ setInterval(function () {
         console.log("are_you_there_click running...")
     }
     document.querySelector("#rc-anchor-container")?.click()
-    dialog = document.querySelector("colab-recaptcha-dialog>div>div>iframe")?.contentWindow.document
+    dialog = document.querySelector("colab-recaptcha-dialog>div>div>iframe")
     if (dialog) {
-        console.log("colab-recaptcha-dialog");
-        console.log(dialog)
+        //console.log("colab-recaptcha-dialog");
+        //console.log(dialog)
+        //console.log(document);
+        /* iframe=window.frames[dialog.name] */
+        //console.log(dialog.document);
+        /* console.log(window.frames)
+        window.frames.values.forEach(frameElement => {
+            console.log(frameElement.document.body);
+        }); */
         dialog?.click()
+        let centerX = dialog.offsetLeft + dialog.offsetWidth / 2;
+        let centerY = dialog.offsetTop + dialog.offsetHeight / 2;
+        var elem=document.elementFromPoint(centerX, centerY)
+        console.log(elem);
+        elem.click()
     }
     document.getElementsByClassName("recaptcha-checkbox goog-inline-block recaptcha-checkbox-unchecked rc-anchor-checkbox")?.item("recaptcha-anchor")?.click()
+
 }, 5000)
 
 // Kaydedilmeyenler var uyarısını engeller
