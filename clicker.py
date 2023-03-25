1498855db2b185cdba948ad46f1f7a74addff8c5
mr-aydemir <aydemir_emre65@hotmail.com>
2023-03-25 20:10:19 +0300

    Update clicker.py

--- clicker.py
+++ clicker.py
@@ -5,7 +5,7 @@ import pyautogui as pg
 screenshot=pg.screenshot()
 screenshot=cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
 
-robo=pg.locateOnScreen('light_robo.png')
+robo=pg.locateOnScreen('white_robo.png')
 cv2.rectangle(screenshot, (robo.left, robo.top), (robo.left+robo.width, robo.top+robo.height), (0,255,255),2)
 
 cv2.imshow("robo", screenshot)
