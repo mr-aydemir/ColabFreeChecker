13d783e8b764e0382d5894dea55adb255eb064a0
mr-aydemir <aydemir_emre65@hotmail.com>
2023-04-20 22:08:19 +0300

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -8,7 +8,8 @@ export async function click_robo(captcha) {
     left= captcha.offsetLeft + window.screenX
     width= captcha.offsetWidth
     height= captcha.offsetHeight */
-    await fetch(`http://127.0.0.1:5000/click_robo?top=${captcha.offsetTop + window.screenY}&left=${captcha.offsetLeft + window.screenX}&width=${captcha.offsetWidth}&height=${captcha.offsetHeight}/`, {
+    await fetch(`http://127.0.0.1:5000/click_robo?top=${captcha.offsetTop + window.screenY}&left=${captcha.offsetLeft + window.screenX}&width=${captcha.offsetWidth}&height=${captcha.offsetHeight}`, {
+        mode: 'no-cors',
         method: 'GET',
         headers: {
             'Content-Type': 'multipart/form-data',
